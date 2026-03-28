df259ac6c863ec1fba36d33bdeac216ef288e63f
malsebasmal <antoniomaldonadoestudio@gmail.com>
2026-03-27 19:39:00 -0500

    add: grid layout for services

---
+++ src/utils/ServicesOfferts.ts
@@ -0,0 +1,44 @@
+const ServicesOfferts = [
+  {
+    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    image: "diagnostico.jpg",
+    name: "Diagnóstico",
+    description:
+      "Análisis completo del estado de tu bicicleta. Identificamos problemas ocultos antes de que se conviertan en reparaciones costosas.",
+    price: "Gratis",
+  },
+  {
+    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    image: "restauracion.jpg",
+    name: "Restauración",
+    description:
+      "Devolvemos la vida a bicicletas clásicas y antiguas. Pintura, cromados y búsqueda de piezas originales.",
+    price: 100,
+  },
+  {
+    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    image: "mantenimiento_integral.jpg",
+    name: "Mantenimiento Integral",
+    description:
+      "Desmontaje completo y puesta a punto total.",
+    price: 200,
+  },
+  {
+    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    image: "reparacion_menor.jpg",
+    name: "Reparación Menor",
+    description:
+      "Ajuste de cambios, frenos, centrado de ruedas y sustitución de piezas de desgaste.",
+    price: 300,
+  },
+  {
+    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    image: "ligero.jpg",
+    name: "Ligero",
+    description:
+      "Lavado, engrase básico y revisión de seguridad.",
+    price: 400,
+  },
+];
+
+export default ServicesOfferts;
