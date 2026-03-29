c58be63c3394a4387f527c2f1807b258ffbddec0
malsebasmal <antoniomaldonadoestudio@gmail.com>
2026-03-29 16:59:56 -0500

    change the layout for the cards

--- src/utils/ServicesOfferts.ts
+++ src/utils/ServicesOfferts.ts
@@ -1,43 +1,43 @@
 const ServicesOfferts = [
   {
-    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
-    image: "diagnostico.jpg",
+    imageSrcBackground: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    imageIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-brush-cleaning-icon lucide-brush-cleaning"><path d="m16 22-1-4"/><path d="M19 14a1 1 0 0 0 1-1v-1a2 2 0 0 0-2-2h-3a1 1 0 0 1-1-1V4a2 2 0 0 0-4 0v5a1 1 0 0 1-1 1H6a2 2 0 0 0-2 2v1a1 1 0 0 0 1 1"/><path d="M19 14H5l-1.973 6.767A1 1 0 0 0 4 22h16a1 1 0 0 0 .973-1.233z"/><path d="m8 22 1-4"/></svg>`,
+    name: "Ligero",
+    description:
+      "Lavado, engrase básico y revisión de seguridad.",
+    price: "Desde S/" + 400,
+  },
+  {
+    imageSrcBackground: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    imageIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>`,
     name: "Diagnóstico",
     description:
       "Análisis completo del estado de tu bicicleta. Identificamos problemas ocultos antes de que se conviertan en reparaciones costosas.",
     price: "Gratis",
   },
   {
-    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
-    image: "restauracion.jpg",
+    imageSrcBackground: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    imageIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-history-icon lucide-history"><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/><path d="M3 3v5h5"/><path d="M12 7v5l4 2"/></svg>`,
     name: "Restauración",
     description:
       "Devolvemos la vida a bicicletas clásicas y antiguas. Pintura, cromados y búsqueda de piezas originales.",
-    price: 100,
+    price: "Desde S/" + 100,
   },
   {
-    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
-    image: "mantenimiento_integral.jpg",
+    imageSrcBackground: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    imageIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bolt-icon lucide-bolt"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/><circle cx="12" cy="12" r="4"/></svg>`,
     name: "Mantenimiento Integral",
     description:
       "Desmontaje completo y puesta a punto total.",
-    price: 200,
+    price: "Desde S/" + 200,
   },
   {
-    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
-    image: "reparacion_menor.jpg",
+    imageSrcBackground: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
+    imageIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-droplets-icon lucide-droplets"><path d="M7 16.3c2.2 0 4-1.83 4-4.05 0-1.16-.57-2.26-1.71-3.19S7.29 6.75 7 5.3c-.29 1.45-1.14 2.84-2.29 3.76S3 11.1 3 12.25c0 2.22 1.8 4.05 4 4.05z"/><path d="M12.56 6.6A10.97 10.97 0 0 0 14 3.02c.5 2.5 2 4.9 4 6.5s3 3.5 3 5.5a6.98 6.98 0 0 1-11.91 4.97"/></svg>`,
     name: "Reparación Menor",
     description:
       "Ajuste de cambios, frenos, centrado de ruedas y sustitución de piezas de desgaste.",
-    price: 300,
-  },
-  {
-    imageSrc: "https://media.istockphoto.com/id/477445662/photo/professional-road-cyclist.jpg?s=612x612&w=0&k=20&c=eFMux1Cr6h0UE5EOgl_XTzwO52ha9hisg0YPDES-Z2A=",
-    image: "ligero.jpg",
-    name: "Ligero",
-    description:
-      "Lavado, engrase básico y revisión de seguridad.",
-    price: 400,
+    price: "Desde S/" + 300,
   },
 ];
 
